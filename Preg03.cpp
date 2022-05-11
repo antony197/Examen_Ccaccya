d86308ae22beee5e884fe465b9f75851946ba2ed
Anselmo Huancas <junior.huancas.leuyacc@gmail.com>
2022-05-11 10:18:58 -0500

    Pregunta 03 - Listo

---
+++ Preg03.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ int num,aux,resto,numInv=0;
+ 
+ cout<<"Ingrese numero"<<endl;
+ cin>>num;
+ 
+ aux=num;
+ 
+ while(aux>0){
+         resto=aux%10;
+      aux=aux/10;
+      numInv=numInv*10+resto;
+ }
+ 
+ if(numInv==num){
+  cout<<"Numero es capicua";
+ }
+ else{
+  cout<<"Numero no capicua";
+  
+ }
+ 
+ return 0;
+}
