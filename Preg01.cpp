6006f2dfb5cb097fe767970c68b4ed5e6935bf0b
Luis Serna <lsernamalca@gmail.com>
2022-05-11 09:59:31 -0500

    Pregunta01 - listo

---
+++ Preg01.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	
+	float n ;
+	float res ; 
+	
+	
+	cout<<" Ingrese el numero hasta donde dara la Sumatoria: " ;
+	cin>> n ;
+	
+	res=((n+4)-4)/(4*(n+4));
+	
+	cout<<"\n EL RESULTADO DE LA SUMATORIA ES: "<<res;
+}
